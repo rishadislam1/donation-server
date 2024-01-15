693482f0234536fd1dff0d2fc1774e0c03eae621
rishadislam1 <rishad.islam158@gmail.com>
2024-01-15 19:45:07 +0600

    server

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 
 
 
@@ -26,16 +29,16 @@ const client = new MongoClient(uri, {
 
   const verifyJWT = (req, res, next)=>{
     const authorization = req.headers.authorization;
-    if(authorization){
+    if(!authorization){
       return res.status(401).send({
         error: true,
-        message: 'Unauthorized Access'
+        message: 'Error Unauthorized Access'
       })
     }
     const token = authorization.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded)=>{
       if(error){
-        res.send({error:true, message: 'Unauthorized Access'})
+        res.status(402).send({error:true, message: 'Unauthorized Access'})
       }
       req.decoded = decoded;
       next();
@@ -50,16 +53,118 @@ const client = new MongoClient(uri, {
 
 const menuCollection = client.db("bistroDb").collection("menu");
 const cartCollection = client.db("bistroDb").collection("carts");
+const userCollection = client.db('bistroDb').collection('users');
 
-// JWT routes
 
-app.post('/jwt', (req,res)=>{
-  const user = req.body;
-  const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: '1h'
-  })
-  res.send({token})
+// verify admin
+
+const verifyAdmin = async(req,res)=>{
+  const email = req.decoded.email;
+  const query = {email: email}
+  const user = await userCollection.findOne(query);
+  if(user?.role !== 'admin'){
+    return res.status(403).send({error: true, message: "Forbidden Message"});
+  }
+  next();
+}
+
+// user related api
+// security
+// 1. use jwt token
+app.get('/users', async (req,res)=>{
+  const result = await userCollection.find().toArray();
+  res.send(result);
 })
+
+
+// security layer
+// emial same
+// admin check
+
+app.get('/user/admin/:email', verifyJWT, async(req,res)=>{
+  const email = req.params.email;
+
+  if(req.decoded.email !== email){
+    res.send({admin: false})
+  }
+
+  const query = {email: email};
+
+  const user = await userCollection.findOne(query);
+  const result = {admin: user?.role === 'admin'}
+  res.send(result);
+})
+
+app.post('/register', async(req,res)=>{
+  const { email, name, password } = req.body;
+  let hashedPassword;
+  if(password){
+    const saltRounds = 10;
+    hashedPassword = await bcrypt.hash(password, saltRounds);
+  }
+  const query = {email: email}
+  const existingUser = await userCollection.findOne(query);
+  const token = jwt.sign({email,name}, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '10h',
+  });
+  if(existingUser){
+    return res.send({accessToken:token,message: 'EMAIL ALREADY EXISTS'})
+  }
+  const result = userCollection.insertOne({
+    email,
+    name,
+    password: hashedPassword || null,
+    role: "user"
+  });
+
+  res.send({ accessToken: token, message: 'Registration successful', result });
+
+})
+
+app.post('/login', async (req,res)=>{
+  const {email,password} = req.body;
+  const user = await userCollection.findOne({email});
+  if(!user){
+    return res.send({message: "Invalid Email"});
+  }
+  const passwordMatch = await bcrypt.compare(password,user.password);
+  if(!passwordMatch){
+    return res.send({message: "Invalid Password"});
+  }
+
+  const token  = jwt.sign({email: user.email, name: user.name}, process.env.ACCESS_TOKEN_SECRET,{
+    expiresIn: "10h"
+  });
+  newUser={
+    _id: user._id,
+    email: user.email,
+    name: user.name,
+    role: user.role
+  }
+
+
+  res.send({ accessToken: token, message: 'Login successful', newUser });
+})
+
+app.patch('/updateuser/:id', async(req,res)=>{
+  const id = req.params.id;
+  const filter = {_id: new ObjectId(id)};
+  const updateDoc = {
+    $set: {
+      role: 'admin'
+    }
+  }
+  const result = await userCollection.updateOne(filter,updateDoc);
+  res.send(result);
+})
+
+
+app.delete('/deleteuser/:id', async (req,res)=>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)};
+  const result = await userCollection.deleteOne(query);
+})
+
 
 // menu code start
 // menu get
@@ -81,13 +186,73 @@ app.get('/menucard', async (req,res)=>{
   res.send(menucard);
 })
 
+app.post('/menu', async(req,res)=>{
+  const data = req.body;
+
+  const result = menuCollection.insertOne(data);
+  res.send(result);
+})
+
 
 // cart collection related work
 
+app.get('/carts', verifyJWT, async(req,res)=>{
+  const email = req.query.email;
+  if(!email){
+    res.send([]);
+  }
+  const decodedEmail = req?.decoded?.email;
+ 
+  if(email!== decodedEmail){
+    res.status(403).send({error:true, message: "Forbidden Access"})
+  }
+  else{
+    const query = {email: email};
+    const result = await cartCollection.find(query).toArray();
+    res.send(result);
+  }
+})
+
+// app.get('/carts', async(req,res)=>{
+//   const email = req.query.email;
+//   if(!email){
+//     res.send([]);
+//   }
+
+
+//     const query = {email: email};
+//     const result = await cartCollection.find(query).toArray();
+//     res.send(result);
+  
+// })
+
+// alternative end
 app.post('/carts', async(req,res)=>{
   const item = req.body;
-  const result = cartCollection.insertOne(item);8
+
+  const result = cartCollection.insertOne(item);
   res.send(result);
+})
+
+app.delete('/carts/:id', async(req,res)=>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)};
+  const result = await cartCollection.deleteOne(query);
+})
+
+// payment related api
+
+app.post('/create-payment-intent', async(req,res)=>{
+  const {price} = req.body;
+  const amount = price*100;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: "usd",
+    payment_method_types: ['card']
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  })
 })
 
 
